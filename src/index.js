3c2f425f20585fa9f24e5d05700e83819905ad0d
Stephan Hoyer <ste.hoyer@gmail.com>
2015-09-18 18:43:13 +0200

    Add get/filter/where/map methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,66 @@
+var Immutable = require('immutable');
+var iMap = Immutable.Map;
+var iList = Immutable.List;
 
+var defaultTableOptions = Immutable.Map({
+  pk: 'id'
+});
+
+var zip = function(key, value) {
+  var obj = {};
+  obj[key] = value;
+  return obj;
+};
 
 module.exports = function(config) {
+
+  function query(options) {
+
+    function attachHandler(handler) {
+      return function(fn) {
+        return query(options.updateIn(['handler'], function(handlers) {
+          return handlers.push(function(result) {
+            return handler(result, fn);
+          });
+        }));
+      };
+    }
+
+    return {
+      _options: options,
+
+      get: function(id) {
+        return query(options.merge({
+          limit: 1,
+          returnArray: false,
+          where: zip(options.get('tableOptions').get('pk'), id)
+        }));
+      },
+
+      where: function(where) {
+        return query(options.merge({
+          where: where
+        }));
+      },
+
+      filter: attachHandler(function(result, fn) {
+        return result.filter(fn);
+      }),
+
+      map: attachHandler(function(result, fn) {
+        return result.map(fn);
+      })
+    };
+  }
+
+  function store(tableName, options) {
+    return query(iMap({
+      tableName: tableName,
+      tableOptions: defaultTableOptions.merge(options),
+      handler: iList(),
+      returnArray: true
+    }));
+  }
+
+  return store;
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,13 +1,62 @@
-
+var expect = require('expect.js');
 var fnORM = require('./index');
 
 describe('init', function() {
-  it('should be initializable', function() {
-    var store = fnORM({
+  var store;
+
+  beforeEach(function() {
+    var dbConfig = {
       client: 'sqlite3',
       connection: {
         filename: "/tmp/fnorm.test.sqlite"
       }
+    };
+    store = fnORM(dbConfig);
+  });
+
+  it('should allow to create user store', function() {
+    var query = store('user', {
+      pk: 'hulu'
     });
+    expect(query._options.get('tableOptions').get('pk')).to.be('hulu');
+    expect(query._options.get('tableName')).to.be('user');
+  });
+
+  it('should create a query to fetch array of items from store', function() {
+    var query = store('user');
+    expect(query._options.get('returnArray')).to.be(true);
+  });
+
+  it('should create a query to fetch one item from store', function() {
+    var query = store('user').get(123);
+    expect(query._options.get('limit')).to.be(1);
+    expect(query._options.get('returnArray')).to.be(false);
+    expect(query._options.getIn(['where', 'id'])).to.be(123);
+  });
+
+  it('should create a query with where to fetch array of items from store', function() {
+    var query = store('user').where({
+      huhu: 'haha'
+    });
+    expect(query._options.get('returnArray')).to.be(true);
+    expect(query._options.getIn(['where', 'huhu'])).to.be('haha');
+  });
+
+  it('should create a query with map/filter funcions', function() {
+    function getId(item) { return item.id; }
+    function isFoo(item) { return item.isFoo; }
+
+    var query = store('user').map(getId);
+
+    expect(query._options.get('handler').size).to.be(1);
+
+    query = query.filter(isFoo);
+    expect(query._options.get('handler').size).to.be(2);
+
+    var getIdHandler = query._options.getIn(['handler', 0]);
+    var isFooHandler = query._options.getIn(['handler', 1]);
+
+    expect(getIdHandler([{id: 'huhu'}])[0]).to.be('huhu');
+    expect(isFooHandler([{isFoo: false}, {isFoo: true}])).have.length(1);
   });
 });
